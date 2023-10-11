2fa8a7f4ce49f7c9107c9b4d9caa8f00dd6ff826
Kumari Durga <singhdurga5698@gmail.com>
2023-10-11 09:58:12 +0530

    maintained responsive design

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1 className="text-7xl font-bold text-center mt-7">BMI Calculator</h1>
-      <div className="container mx-auto pt-3 bg-slate-900 text-white md:w-[36rem] md:h-[32rem] mt-[4rem] sm:w-[18rem] sm:h-[38rem]">
+      <div className="container mx-auto pt-3 bg-slate-900 text-white md:w-[36rem] mt-[4rem] sm:w-[18rem]">
         <BmiCalculator />
       </div>
     </>
